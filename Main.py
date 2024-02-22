55c6067dde15b01a34c4cb8caefcfef4e4b5b345
binhleqs <binhuuuo96@gmail.com>
2024-02-22 10:52:04 +0700

    modify main

--- Main.py
+++ Main.py
@@ -9,7 +9,26 @@ from PyDictionary import PyDictionary
 randomWords = random_word.RandomWords()
 dict = PyDictionary()
 
-word = randomWords.get_random_word()
-meaning = dict.meaning(word)
+#generate a simple words
+result = None
+while result is None: 
+    try:
+        word = randomWords.get_random_word()
+        meanings = dict.meaning(word)
+        
+        if len(word) <= 6 and meanings != None :
+            meaning = meanings.get("Noun")
+            result = True
+    except:
+        print("There was a problem")
+
+
 print(word)
-print(meaning.get("Noun")[0])
+num_letter = len(word)
+# get the first meaning in dictionary
+
+print("The word has {} letters".format(num_letter))
+print("□"*num_letter)
+print(meaning)
+
+ 
